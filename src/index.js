1f1e153a2125c89ae2ba77439ff0dcd159b6adf6
Kishore S <87610640+KISHORExlr8@users.noreply.github.com>
2023-08-05 19:51:13 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const Onepiece = ["Ace", "Sabo", "Luffy"];
+Onepiece.pop();
+
+const animals = ["pigs", "goats", "sheep"];
+animals.push("chickens", "cats", "dogs");
+console.log(animals);
+
+const array = [1, 4, 9, 16];
+const map = array.map((x) => x * 2);
+console.log(map);
+
+const array1 = ["a", "b", "c"];
+const array2 = ["d", "e", "f"];
+const array3 = array1.concat(array2);
+console.log(array3);
